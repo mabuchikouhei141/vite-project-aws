512a5c5011b863290cde97f1d88722a13fa46dcc
間渕航平 <mabuchikouhei@MacBook-Air.local>
2023-09-07 14:09:03 +0900

    new

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello AWS</h1>
+      <h1>Hello vite + React + AWS</h1>
       
     </>
   )
